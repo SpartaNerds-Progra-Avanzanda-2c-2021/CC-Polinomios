26e519e946eb9a275029e114316a37f33da7f939
Omar <omar_quispe_95@hotmail.com.ar>
2021-10-12 09:05:16 -0300

    Innitial commit

---
+++ src/Polinomio.java
@@ -0,0 +1,48 @@
+
+public class Polinomio {
+	private int grado;
+	private double[] coeficientes;
+
+	// La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n
+	// la posicion n contiene al termino independiente.
+
+	public Polinomio(double[] coeficientes) {
+		this.coeficientes = coeficientes;
+		this.grado = coeficientes.length;
+	}
+
+	double evaluarMSucesivas(double x) {
+		// TODO
+		return 0;
+	}
+
+	double evaluarRecursiva(double x) {
+		// TODO
+		return 0;
+	}
+
+	double evaluarRecursivaPar(double x) {
+		// TODO
+		return 0;
+	}
+
+	double evaluarProgDinamica(double x) {
+		// TODO
+		return 0;
+	}
+
+	double evaluarMejorada(double x) {
+		// TODO
+		return 0;
+	}
+
+	double evaluarPow(double x) {
+		// TODO
+		return 0;
+	}
+
+	double evaluarHorner(double x) {
+		// TODO
+		return 0;
+	}
+}
